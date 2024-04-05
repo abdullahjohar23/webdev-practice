c365ac84338555580bd621e4dd6cf467fefb9e75
Abdullah Johar <jrrabdullah23@gmail.com>
2024-04-05 17:21:40 +0600

    hero section updated

--- Making Webpage using Tailwind/tailwind.config.js
+++ Making Webpage using Tailwind/tailwind.config.js
@@ -9,9 +9,9 @@ module.exports = {
       center: true,
     },
     screens: {
-      'tablet': '640px', // => @media (min-width: 640px) { ... }
-      'laptop': '1024px', // => @media (min-width: 1024px) { ... }
-      'desktop': '1280px', // => @media (min-width: 1280px) { ... }
+      sm: '640px', // => @media (min-width: 640px) { ... }
+      lg: '1024px', // => @media (min-width: 1024px) { ... }
+      xl: '1280px', // => @media (min-width: 1280px) { ... }
     },
     extend: {},
   },
