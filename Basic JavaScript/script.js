9069aaf808bbad520594bb01159f9e8b5285899c
Abdullah Johar <jrrabdullah23@gmail.com>
2024-04-15 21:34:50 +0600

    updated

--- Basic JavaScript/script.js
+++ Basic JavaScript/script.js
@@ -104,3 +104,144 @@ if (letter === number) {
 } else {
     console.log('number and character are not equal');
 }
+
+// normal for loop
+for (let i = 1; i <= 10; i++) {
+    console.log('what up folks');
+}
+
+// for-of loop (used for string & arrays)
+let s = "abullah";
+for (let i of s) {
+    console.log(`i = ${i}`);
+}
+
+// for-in loop (used for objects)
+let student = {
+    Name: "Abdullah",
+    age: 24,
+    cgpa: 3.30,
+    isPassed: true
+};
+
+for (let key in student) {
+    console.log(`key = ${key} & value = ${student[key]}`);
+}
+
+// A simple guessing game
+let correctAnswer = 5;
+let userNInput = prompt('Guessa a number between 1 to 20');
+
+// don't put !== becase prompt always works as string
+while (userNInput != correctAnswer) {
+    userNInput = prompt('Wrong guess, try again');
+}
+
+console.log(`Yes! ${userNInput} is the correct answer`);
+
+// Mutable vs Immutable
+
+// Strings are immutable
+let str = 'abc';
+console.log(str[1]);
+str[1] = 'c'; // s[1] won't convertt into c
+console.log(s[1]);
+
+// Arrays are mutable
+let arr = [1, 2, 'a'];
+console.log(arr[1]);
+arr[1] = 5;
+console.log(arr[1]);
+
+// Array Methods
+let food = ["potato", "cheese", "tomato", "butter"];
+console.log(food);
+
+food.push("eggplant")
+console.log(food);
+
+food.pop();
+console.log(food);
+
+console.log(food.toString());
+
+let bucket = ["honey", "ketchup"];
+let allFood = food.concat(bucket);
+console.log(allFood);
+
+food.push("mayo");
+
+// (push vs unshift) & (pop vs shift)
+let list = ["potato", "cheese", "tomato"];
+console.log(list);
+
+list.push("butter"); // pushes from end
+console.log(list);
+
+list.unshift("honey"); // pushes from start
+console.log(list);
+
+list.pop(); // pops from end
+console.log(list);
+
+list.shift(); // pops from start
+console.log(list);
+
+// slice()d splice()
+let nums = [0, 1, 2, 3, 4, 5, 6, 7];
+console.log(nums);
+
+// slice(start, end)
+console.log(nums.slice(1, 4));
+
+console.log(nums);
+
+// splice(startIndex, deleteCount, newElements)
+
+// delete and add using splice()
+nums.splice(2, 3, 101, 102); // go to index 2 & delete index 2, 3, 4 and add 101, 102 from index 2
+console.log(nums);
+
+// add using splice()
+nums.splice(4, 0, 103, 104); // go to index 4 & delete nothing and add 103, 104 from index 4
+console.log(nums);
+
+// delete using splice()
+nums.splice(2, 4); // go to index 4 & delete nothing
+console.log(nums);
+
+// Function vs Arrow Function
+function sum (a, b) {
+    return a + b;
+}
+
+const arrowSum = (a, b) => {
+    return a + b;
+}
+
+
+// forEach (it takes a function as its parameter)
+let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// val means each value at each index
+arr.forEach (function printVal(val) {
+    console.log(val);
+})
+
+// this is how you can code using arrow functions
+arr.forEach ((val) => {
+    console.log(val);
+})
+
+// arr.forEach(callback)
+// callback function has 3 parameters (value, index, array)
+arr.forEach (function printVal(val, ind, arr) {
+    console.log(val, ind, arr);
+})
+
+// writing the function outside
+let printVal = (val) => {
+    console.log(val);
+}
+
+arr.forEach(printVal);
